69d0bccd8630e433f271e0b981aaa50cd0312cb2
Azhar Uddin <azhar.cu25@gmail.com>
2026-06-20 04:30:55 +0600

    comment: on other future statuses

--- app/FlightSearch/Enums/BookingStatus.php
+++ app/FlightSearch/Enums/BookingStatus.php
@@ -5,4 +5,5 @@ namespace App\FlightSearch\Enums;
 enum BookingStatus: string
 {
     case CONFIRMED = 'confirmed';
+    // other statuses like pending, cancelled, etc. can be added here as needed
 }
