fedb27ba59b380633d7452a951d7b33070438d95
Azhar Uddin <azhar.cu25@gmail.com>
2026-06-20 15:11:26 +0600

    Revert "fix: move OpenAPI doc route to /apidocs outside api prefix"
    This reverts commit 8cf21790a5bba0e3e77e44467690fa6646730d5f.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,14 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        then: function () {
-            Route::get('/apidocs', function () {
-                return response()->file(
-                    storage_path('api-docs/api.yaml'),
-                    ['Content-Type' => 'application/x-yaml'],
-                );
-            });
-        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
